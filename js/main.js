a1b3d4871dde271da4e70a01319de27ae1490b75
jenishls <jlshrestha90@gmail.com>
2018-03-26 12:27:46 +0545

    created ajax function

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function sendAjax(url,data,method,callback = ''){
+
+	$.ajax({
+
+		url:url,
+		data:data,
+		method:method,
+		success:function(response){
+			if(typeof(callback) != "string"){
+				callback(response);
+			}
+		}
+	});
+
+}
+
+
+
